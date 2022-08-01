47c1cb11c778475beeff5d0eb5f2ef56427413cb
Alekosh <aakhosh00@gmail.com>
2022-08-02 00:45:59 +0500

    full accordion

---
+++ js/main.js
@@ -0,0 +1,53 @@
+// let tabs__header = document.querySelectorAll(".tab__header-item");
+// let tab__body = document.querySelectorAll(".tab__body-item");
+
+// // old
+// tabs__header.forEach(function (tab, tab_index) {
+//   tab.addEventListener("click", function () {
+
+//     tabs__header.forEach(function (tab) {
+//       tab.classList.remove("active"); //Сначала удаляем у всех класс active
+//     });
+
+//     tab.classList.add("active"); //Затем добавяем класс active по нажатию
+
+//     tab__body.forEach(function (content, content_index) {
+//       if (content_index == tab_index) {
+//         content.style.display = "block";
+//       }
+//       else {
+//         content.style.display = "none";
+//       }
+//     });
+
+//   });
+
+// });
+
+// new ↓↓↓
+// tabs__header.forEach((tab, index) => {
+
+//   tab.addEventListener('click', () => {
+
+//     tabs__header.forEach((tab) => {
+//       tab.classList.remove('active');
+//     });
+
+//     tabs__header[index].classList.add('active');
+
+//     tab__body.forEach((content) => {
+//       content.classList.remove('is-active');
+//     });
+
+//     tab__body[index].classList.add('is-active');
+
+//   });
+
+// });
+
+// for (let i = 0; i < accordion.length; i++) {
+    //   accordion[i].addEventListener('click', function () {
+    //     this.classList.toggle('active');
+    //   });
+
+    //}
